064731a86fa4aba50251198457a34dacf909b613
Kristian Larsson <kristian@tutamail.com>
2017-08-16 11:52:36 -0700

    brute force sum

---
+++ lib/euler_sum.ex
@@ -0,0 +1,16 @@
+defmodule EulerSum do
+  
+  def while(sum, cur, _prev) when cur >= 4000000 do
+    sum      
+  end  
+  
+  def while(sum, cur, prev) do
+    next = prev + cur
+    while(sum + next * (1 - rem(next, 2)), next, cur)
+  end
+
+  def solve_brute do
+    while(0, 1, 1)
+  end  
+
+end
