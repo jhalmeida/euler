82d391a2bb8f92106a6688ed943a289d046279a8
Kristian Larsson <kristian@tutamail.com>
2017-08-25 14:42:24 -0700

    optimized, removed is_prime since we will find out by finding the next factor

--- lib/003.ex
+++ lib/003.ex
@@ -1,39 +1,26 @@
 defmodule PrimeFactor do
  
-  defp is_prime(number, factor) when factor > number / 2 do true end
-
-  defp is_prime(number, factor) do
-    cond do
-      rem(number, factor) == 0 ->
-        IO.inspect(factor, label: "factor for non prime")
-        false
-      true ->
-        is_prime(number, factor + 1)
-    end
+  defp check_factor(number, factor, highest_factor) when factor > number / 2 do
+    IO.puts "=========================="
+    highest_factor
   end
 
-  defp check_factor(number, factor) when factor > number / 2 do "No prime factor" end
-
-  defp check_factor(number, factor) do
+  defp check_factor(number, factor, highest_factor) do
     kvot = div(number, factor)
     cond do
       rem(number, factor) == 0 ->
+        IO.puts "--------------------------"
         IO.inspect(kvot, label: "factor found, kvot")
         IO.inspect(factor, label: "factor found, factor")
-        cond do
-          is_prime(kvot, 2) ->
-            kvot
-          true ->
-            check_factor(kvot, 2)
-        end
+        check_factor(kvot, 2, kvot)
       true ->
-        check_factor(number, factor + 1)
+        check_factor(number, factor + 1, highest_factor)
     end
   end
 
   def solve(number) do
     IO.inspect(number, label: "Number")
-    check_factor(number, 2)
+    check_factor(number, 2, 1)
     |> IO.inspect(label: "Highest prime factor")
   end
 end
