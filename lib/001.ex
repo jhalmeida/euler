9088e0659c6f0d6b04b21f0fa28f04295e2ecf1c
Kristian Larsson <kristian@tutamail.com>
2017-08-31 09:29:49 -0700

    changed limit

--- lib/001.ex
+++ lib/001.ex
@@ -3,7 +3,7 @@ defmodule One do
   
   def solve_brute do
     {_list, answer} =
-      Enum.map_reduce(1..1000, 0,  fn(x, acc) -> 
+      Enum.map_reduce(1..999, 0,  fn(x, acc) ->
         cond do
           rem(x,3) == 0 || rem(x, 5) == 0 ->
             { x, acc + x}
@@ -19,8 +19,8 @@ defmodule One do
     m = div(limit, 5)
     l = div(limit, 15)
     three_sum = div(3 * (n * (n+1)), 2)
-    five_sum = div(5 * (m * (m+1)),2)
-    fift_sum = div(15 * (l * (l+1)),2)
+    five_sum = div(5 * (m * (m+1)), 2)
+    fift_sum = div(15 * (l * (l+1)), 2)
     three_sum + five_sum - fift_sum
   end
 
