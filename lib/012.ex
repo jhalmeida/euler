603a4a0ab83c82eda8996242285c02cf024e760b
Kristian Larsson <kristian@tutamail.com>
2018-03-30 11:23:37 -0700

    added check for square numbers

--- lib/012.ex
+++ lib/012.ex
@@ -1,12 +1,23 @@
 defmodule Twelve do
 
-  @factors 4
+  @factors 10
 
-  def get_factors(n, l) when l > @factors do
-    n * (n+1) / 2 |> Kernel.trunc()
+  def get_factors(n, l, len) when len > 9 do
+    confirm_len =
+      l
+      |> List.flatten
+      |> Enum.uniq
+      |> length
+
+    case confirm_len > @factors do
+      true -> n * (n+1) / 2 |> Kernel.trunc()
+      false ->
+        IO.inspect(%{triangle: n * (n+1) / 2 |> Kernel.trunc(), n: n, hacky_len: len, confirm_len: confirm_len, factors: l}, label: "didn't meet expected length")
+        get_factors(n, [], 0)
+    end
   end
 
-  def get_factors(n, l) when l <= @factors do
+  def get_factors(n, _l, _len) do
     n = n + 1
     tn = n * (n + 1) / 2 |> Kernel.trunc()
     last =
@@ -15,10 +26,10 @@ defmodule Twelve do
       |> Kernel.trunc()
 
     l = for x <- 1..last, rem(tn, x) == 0, do: [x, div(tn, x)]
-    get_factors(n, length(l) * 2)
+    get_factors(n, l, length(l) * 2)
   end
 
   def find_triangle_number do
-    get_factors(1, 0)
+    get_factors(1, [], 0)
   end
 end
