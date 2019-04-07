705fa6f9206ab1ce8f09735fe767e5236fbcddda
Kristian Larsson <kristian@tutamail.com>
2019-04-07 15:39:56 +0200

    updated elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Euler.Mixfile do
   def project do
     [app: :euler,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
