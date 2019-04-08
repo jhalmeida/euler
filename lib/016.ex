4c503352ed400ab95c2b6a9dc84229c8871737cc
Kristian Larsson <kristian@tutamail.com>
2019-04-08 13:43:43 +0200

    solved 16

--- lib/016.ex
+++ lib/016.ex
@@ -1,7 +1,16 @@
 defmodule Sixteen do
-  """
+  @moduledoc """
   2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
   What is the sum of the digits of the number 2^1000?
   """
+  def exp(base, 1), do: base
+  def exp(base, pow), do: base * exp(base, pow - 1)
+
+  def solve(pow \\ 15) do
+    exp(2, pow)
+    |> Integer.to_string(10)
+    |> String.graphemes()
+    |> Enum.reduce(0, &(String.to_integer(&1) + &2))
+  end
 end
