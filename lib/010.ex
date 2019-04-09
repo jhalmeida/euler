d9f0ef59ef1284169e54631f838e6e032334655f
Kristian Larsson <kristian@tutamail.com>
2019-04-09 15:11:35 +0200

    uncommented solution

--- lib/010.ex
+++ lib/010.ex
@@ -1,24 +1,23 @@
-# defmodule Ten do
+defmodule Ten do
 
-#   defp filter([head | tail], sum, limit) when head > limit do
-#     Enum.reduce([sum, head | tail], fn(x, acc) -> x + acc end)
-#   end
+  defp filter([head | tail], sum, limit) when head > limit do
+    Enum.reduce([sum, head | tail], fn(x, acc) -> x + acc end)
+  end
 
-#   defp filter([head | tail], sum, limit) do
-#     Enum.reject(tail, fn(x) -> rem(x, head) == 0 end)
-#       |> filter(sum + head, limit)
-#   end
+  defp filter([head | tail], sum, limit) do
+    Enum.reject(tail, fn(x) -> rem(x, head) == 0 end)
+      |> filter(sum + head, limit)
+  end
 
-#   def solve do
-#     start_time = :os.system_time(:milli_seconds)
-#     limit = 10
-#     list = Enum.map(1..(div(limit, 2) - 1), fn x -> x * 2 + 1 end)
-#     filter(list, 2, :math.sqrt(limit))
-#       |> IO.inspect(label: "Result")
-#     end_time = :os.system_time(:milli_seconds)
-#     IO.puts("Time taken: #{(end_time - start_time)/1000}s")
-#   end
-
+  def solve(limit \\ 10) do
+    start_time = :os.system_time(:milli_seconds)
+    list = Enum.map(1..(div(limit, 2) - 1), fn x -> x * 2 + 1 end)
+    filter(list, 2, :math.sqrt(limit))
+      |> IO.inspect(label: "Result")
+    end_time = :os.system_time(:milli_seconds)
+    IO.puts("Time taken: #{(end_time - start_time)/1000}s")
+  end
+end
 
 
 
